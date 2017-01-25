d8f24420a155f11c7e5f915f25b9262674f9e073
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-01-25 17:07:16 +0700

    infinite scroll done

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,13 +12,14 @@ angular.module('myApp.view1', ['ngRoute', 'infinite-scroll'])
 
 .controller('View1Ctrl', function($scope, $http){
     $scope.page = 1;
+    $scope.details = [];
     $http.get("http://api.themoviedb.org/3/discover/movie?api_key=b83e15027df50325aa48d0cdc5c9bf30&&page="+$scope.page)
-        .then(function(response){ $scope.details = response.data; });
+        .then(function(response){ $scope.details.push(response.data); });
     $scope.nextPage = function(){
         $scope.busy = true;
         $scope.page += 1;
         $http.get("http://api.themoviedb.org/3/discover/movie?api_key=b83e15027df50325aa48d0cdc5c9bf30&&page="+$scope.page)
-            .then(function(response){ $scope.details = response.data; });
+            .then(function(response){ $scope.details.push(response.data); });
         $scope.busy = false;
     }
 });
