efec434a6f88ef22ed9ba346e4cca47c50296006
Ki Ageng Satria Pamungkas <pamungkas.kiagengsatria@outlook.com>
2017-01-25 17:40:57 +0700

    adding overview and fixing infinite scroll

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -13,13 +13,22 @@ angular.module('myApp.view1', ['ngRoute', 'infinite-scroll'])
 .controller('View1Ctrl', function($scope, $http){
     $scope.page = 1;
     $scope.details = [];
+    $scope.info = [];
+    $scope.busy = false;
     $http.get("http://api.themoviedb.org/3/discover/movie?api_key=b83e15027df50325aa48d0cdc5c9bf30&&page="+$scope.page)
         .then(function(response){ $scope.details.push(response.data); });
     $scope.nextPage = function(){
+        if ($scope.busy) return;
         $scope.busy = true;
         $scope.page += 1;
         $http.get("http://api.themoviedb.org/3/discover/movie?api_key=b83e15027df50325aa48d0cdc5c9bf30&&page="+$scope.page)
-            .then(function(response){ $scope.details.push(response.data); });
-        $scope.busy = false;
+            .then(function(response){ $scope.details.push(response.data);
+                $scope.busy = false; });
+    };
+    $scope.showMoreInfo = function(index){
+        $scope.info[index] = !$scope.info[index];
+    }
+    $scope.noIndex = function(index, noPage){
+        return ((noPage-1)*20) + index;
     }
 });
